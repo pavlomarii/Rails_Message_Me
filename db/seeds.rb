26c2c8a268499cf6f55e44c6b40e35cddd249f48
pavlomarii <pmarii@cybercraftinc.com>
2021-09-23 13:53:44 +0300

    Built User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Pavlo", password: "11111111")
+User.create(username: "Danylo", password: "11111111")
+User.create(username: "Artem", password: "11111111")
+User.create(username: "Oleksiy", password: "11111111")
+User.create(username: "Andriy", password: "11111111")
+User.create(username: "Eugene", password: "11111111")
+User.create(username: "Natalia", password: "11111111")
